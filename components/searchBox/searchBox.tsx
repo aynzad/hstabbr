3cb490ae0cf18c7778e40d16dc6d310c2e2f8b16
Alireza Esfahani <aynzad@gmail.com>
2023-03-26 12:19:25 +0300

    🩻 add words wrapper skeleton

--- components/searchBox/searchBox.tsx
+++ components/searchBox/searchBox.tsx
@@ -2,9 +2,9 @@ import { Suspense } from 'react'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@lib/auth/options'
 import SearchInput from './searchInput'
-import SearchItemSkeleton from './searchItemSkeleton'
 import RecentWordsWrapper from './recentWordsWrapper'
 import CommonWordsWrapper from './commonWordsWrapper'
+import WordsWrapperSkeleton from './wordsWrapperSkeleton'
 
 async function SearchBox() {
   const session = await getServerSession(authOptions)
@@ -15,34 +15,12 @@ async function SearchBox() {
       <div className="flex-col glass overflow-hidden">
         <SearchInput email={email} />
         {email && (
-          <Suspense
-          // fallback={
-          //   <div className="border-b border-b-white py-3 px-6 border-opacity-[0.16]">
-          //     <h6 className="text-left text-base font-medium text-primary-dark my-2">
-          //       Most Recent
-          //     </h6>
-          //     <SearchItemSkeleton />
-          //     <SearchItemSkeleton />
-          //     <SearchItemSkeleton />
-          //   </div>
-          // }
-          >
+          <Suspense fallback={<WordsWrapperSkeleton title="Most Recent" />}>
             {/* @ts-expect-error Server Component */}
             <RecentWordsWrapper email={email} />
           </Suspense>
         )}
-        <Suspense
-          fallback={
-            <div className="py-3 px-6">
-              <h6 className="text-left text-base font-medium text-primary-dark my-2">
-                Most Common
-              </h6>
-              <SearchItemSkeleton />
-              <SearchItemSkeleton />
-              <SearchItemSkeleton />
-            </div>
-          }
-        >
+        <Suspense fallback={<WordsWrapperSkeleton title=" Most Common" />}>
           {/* @ts-expect-error Server Component */}
           <CommonWordsWrapper />
         </Suspense>
---
+++ components/searchBox/wordsWrapperSkeleton.tsx
@@ -0,0 +1,20 @@
+import SearchItemSkeleton from './searchItemSkeleton'
+
+interface Props {
+  title: string
+}
+
+function WordsWrapperSkeleton({ title }: Props) {
+  return (
+    <div className="border-b border-b-white py-3 px-6 border-opacity-[0.16]">
+      <h6 className="text-left text-base font-medium text-primary-dark my-2">
+        {title}
+      </h6>
+      <SearchItemSkeleton />
+      <SearchItemSkeleton />
+      <SearchItemSkeleton />
+    </div>
+  )
+}
+
+export default WordsWrapperSkeleton
