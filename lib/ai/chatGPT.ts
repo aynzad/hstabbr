93ae416f768e40d902637a193b7b5dd97543bfb4
Alireza Esfahani <aynzad@gmail.com>
2023-03-29 14:09:48 +0300

    🐞 fix categories normalization issue

--- lib/ai/chatGPT.ts
+++ lib/ai/chatGPT.ts
@@ -17,10 +17,10 @@ export async function generateWord(input: Input) {
 
     const definition = input.definition?.toLocaleLowerCase() ?? (await chatGPT.sendMessage(`What "${abbreviation}" is stand for? respond without any explanation , just the definition`)).text.toLocaleLowerCase()
     const descriptionResponse = await chatGPT.sendMessage(`Write a description in maximum of 3 sentences (max of 350 char) about this word:"${definition}". Try to be funny and sarcastic but keep the tune formal`)
-    const categoryResponse = await chatGPT.sendMessage(`Suggest up to 3 simple categories for this word "${abbreviation}" meaning "${definition}", each category should be kebab case without space or anything, write these category in one line split them with a comma (,)`)
+    const categoryResponse = await chatGPT.sendMessage(`Suggest up to 3 simple categories for this word "${abbreviation}" meaning "${definition}", each category should be a simple word, write these category in one line split them with a comma (,)`)
 
     const normalizedDefinition = definition.replaceAll('"', '').replaceAll("'", '').replaceAll(abbreviation, '').replaceAll('stands for', '').replaceAll('.', '').replaceAll(':', '').trim()
-    const normalizedCategories = categoryResponse.text.toLocaleLowerCase().replaceAll('_', '-').trim().replaceAll(' ', '-').trim()
+    const normalizedCategories = categoryResponse.text.toLocaleLowerCase().replaceAll(', ', ',').replaceAll('_', '-').trim().replaceAll(' ', '-').trim().replaceAll(',', ', ')
 
     const word: RawSimpleWord = {
         id: descriptionResponse.id,
